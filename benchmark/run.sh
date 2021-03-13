46faaabf1a09b3c91d8e2c9813427ac23a0da8e2
rtmigo <art.galkin@gmail.com>
2021-03-13 17:34:53 +0300

    stub benchmark

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e && cd ${0%/*}
+set -e #&& cd ${0%/*}
 
 mkdir -p build
 rm -rf build/benchmark.com || true
-dart compile exe benchmark.dart -o build/benchmark.com
-build/benchmark.com
+#dart compile exe bin/cli.dart -o build/benchmark
+dart compile exe bin/cli.dart -o build/benchmark.exe
+build/benchmark.exe
+#build/benchmark
 
 #dart compile exe
 #dart2native benchmark.dart -o build/benchmark.com
