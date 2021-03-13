3decee6567833cc5a8d475bb12cd542015335c6c
rtmigo <art.galkin@gmail.com>
2021-03-13 17:59:38 +0300

    bench

--- benchmark/bin/cli.dart
+++ benchmark/bin/cli.dart
@@ -1,7 +1,29 @@
+import 'dart:math';
+
 import 'package:cli/cli.dart' as cli;
 import 'package:xorshift/xorshift.dart';
 
+int measureTime(Random r)
+{
+  final sw = Stopwatch()..start();
+
+  for (var i = 0; i < 100000; ++i) {
+    r.nextBool();
+  }
+  return sw.elapsed.inMilliseconds;
+}
+
 void main(List<String> arguments) {
-  final r = Xorshift();
-  print('Hello world: ${r.next()}!');
+
+  final results = <String,List<int>>{};
+
+  results.putIfAbsent('128+', () => <int>[]).add(measureTime(Xorshift128Plus.deterministic()));
+
+  for (final entry in results.entries)
+    {
+      print('{${entry.key} ${entry.value}');
+    }
+
+  //final r = Xorshift128Plus(1,2);
+  //print('Hello $xxxx world: ${r.next()}!');
 }
--- lib/xorshift.dart
+++ lib/xorshift.dart
@@ -7,5 +7,11 @@ export 'src/xorshift128plus.dart' show Xorshift128Plus;
 export 'src/xorshift128.dart' show Xorshift128;
 export 'src/xorshift64.dart' show Xorshift64;
 export 'src/xorshift32.dart' show Xorshift32;
+//export 'src/unirandom.dart' show Xorshift32;
 
-mixin Xorshift on Xorshift128Plus {}
+//class Xorshift extends Xorshift128Plus {}
+
+// mixin Xorshift on Xorshift128Plus {
+//   int z=1;
+// }
+//int xxxx=1;
