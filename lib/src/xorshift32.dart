d0f5f03c5ab660e1215d5e1a8efd4ea762fdc13f
rtmigo <art.galkin@gmail.com>
2021-03-15 20:16:01 +0300

    returned to the default Xorshift32 bool

--- lib/src/xorshift32.dart
+++ lib/src/xorshift32.dart
@@ -46,19 +46,19 @@ class Xorshift32 extends RandomBase32
     return _state = x;
   }
 
-  @override
-  bool nextBool() {
-    // xorshift32 is so fast, that even returning particular
-    // bits is not faster than nextInt32()
-
-    var x = _state;
-
-    x ^= (x << 13);
-    x &= 0xFFFFFFFF; // added
-    x ^= (x >> 17);
-    x ^= (x << 5);
-    x &= 0xFFFFFFFF; // added
-
-    return (_state = x) >= 0x80000000;
-  }
+  // @override
+  // bool nextBool() {
+  //   // xorshift32 is so fast, that even returning particular
+  //   // bits is not faster than nextInt32()
+  //
+  //   var x = _state;
+  //
+  //   x ^= (x << 13);
+  //   x &= 0xFFFFFFFF; // added
+  //   x ^= (x >> 17);
+  //   x ^= (x << 5);
+  //   x &= 0xFFFFFFFF; // added
+  //
+  //   return (_state = x) >= 0x80000000;
+  // }
 }
