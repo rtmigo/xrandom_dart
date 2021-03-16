64a166b7f542b9503668c7118264c5f176fb9e31
rtmigo <art.galkin@gmail.com>
2021-03-16 18:32:58 +0300

    docs

--- lib/src/xorshift128plus.dart
+++ lib/src/xorshift128plus.dart
@@ -7,6 +7,10 @@ import 'package:xrandom/src/splitmix64.dart';
 import '00_errors.dart';
 import '00_ints.dart';
 
+enum Xorshift128pConstants {
+  c23_18_5
+}
+
 /// Random number generator based on `xorshift128+` algorithm by S.Vigna (2015).
 /// The reference implementation in C can be found in <https://arxiv.org/abs/1404.0390> (V3).
 class Xorshift128p extends RandomBase64 {
@@ -45,6 +49,23 @@ class Xorshift128p extends RandomBase64 {
     // https://arxiv.org/abs/1404.0390 [v2] Mon, 14 Dec 2015 - page 6
     // https://arxiv.org/abs/1404.0390 [v3] Mon, 23 May 2016 - page 6
 
+    // There was at least two versions of the constants used in algorithm.
+    // 23, 17, 26
+    //  - in paper by Vigna https://arxiv.org/pdf/1404.0390v1.pdf - Apr 2014
+    //  - in JavaScript V8 engine https://git.io/Jqpma
+    //  - on Wikipedia https://en.wikipedia.org/wiki/Xorshift
+    //
+    // 23, 18, 5
+    //  - in paper by Vigna https://arxiv.org/pdf/1404.0390v2.pdf - Dec 2015
+    //  - in JavaScript xorshift library <https://git.io/JqWCP>
+
+    // "the most recent set of constants according to the author of the algorithm are:
+    // 23, 18, and 5. Apparently it doesn't matter too much, but those are theoretically
+    // better than the initial set of numbers" <https://stackoverflow.com/a/34432126>
+
+
+    // https://stackoverflow.com/questions/34426499/what-is-the-real-definition-of-the-xorshift128-algorithm
+
     var s1 = _S0;
     final s0 = _S1;
     final result = s0 + s1;
@@ -56,12 +77,12 @@ class Xorshift128p extends RandomBase64 {
 
     _S1 = s1 ^
         s0 ^
-        ( // V1: s1.unsignedRightShift(18)
-            // V2: s1 >= 0 ? s1 >> 18 : ((s1 & INT64_MAX_POSITIVE) >> 18) | (1 << (63 - 18))
-            (s1 >> 18) & ~(-1 << (64 - 18))) ^
-        ( // V1: s0.unsignedRightShift(5)
-            // V2: s0 >= 0 ? s0 >> 5 : ((s0 & INT64_MAX_POSITIVE) >> 5) | (1 << (63 - 5))
-            (s0 >> 5) & ~(-1 << (64 - 5))); // b, c
+        ( // rewritten: s1.unsignedRightShift(18)
+            (s1 >> 18) & ~(-1 << (64 - 18))
+        ) ^
+        ( // rewritten: s1 s0.unsignedRightShift(5)
+            (s0 >> 5) & ~(-1 << (64 - 5))
+        ); // b, c
 
     return result;
   }
