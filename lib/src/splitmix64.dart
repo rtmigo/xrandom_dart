91dfeecf3c48f614b0c48ba0dc7f043a0a23bb42
rtmigo <art.galkin@gmail.com>
2021-03-16 04:45:05 +0300

    splitmix should pass node tests now

--- lib/src/splitmix64.dart
+++ lib/src/splitmix64.dart
@@ -27,6 +27,10 @@ class Splitmix64 extends RandomBase64 {
   }
 
   late int _x;
+  
+  static final _c9E3 = int.parse('0x9e3779b97f4a7c15');
+  static final _cBF5 = int.parse('0xbf58476d1ce4e5b9');
+  static final _c94D = int.parse('0x94d049bb133111eb');
 
   @override
   int nextInt64() {
@@ -34,11 +38,11 @@ class Splitmix64 extends RandomBase64 {
     // https://prng.di.unimi.it/splitmix64.c
     // (c) 2015 by Sebastiano Vigna (CC-0)
 
-    int z = (_x += 0x9e3779b97f4a7c15);
+    int z = (_x += _c9E3);
     // C99: z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
-    z = (z ^ (z >> 30) & ~(-1 << (64 - 30))) * 0xbf58476d1ce4e5b9;
+    z = (z ^ (z >> 30) & ~(-1 << (64 - 30))) * _cBF5;
     // C99: z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
-    z = (z ^ (z >> 27) & ~(-1 << (64 - 27))) * 0x94d049bb133111eb;
+    z = (z ^ (z >> 27) & ~(-1 << (64 - 27))) * _c94D;
     // C99: return z ^ (z >> 31);
     return z ^ (z >> 31) & ~(-1 << (64 - 31));
   }
