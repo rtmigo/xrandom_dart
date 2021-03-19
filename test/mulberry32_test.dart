9af26cb9179c1679bfcf559f21b03d5dcbaec398
rtmigo <art.galkin@gmail.com>
2021-03-19 07:45:26 +0300

    mulberry works

--- test/mulberry32_test.dart
+++ test/mulberry32_test.dart
@@ -8,9 +8,42 @@ import 'package:xrandom/src/60_xorshift32.dart';
 import 'helper.dart';
 
 void main() {
-  //testCommonRandom(() => Mulberry32(), ()=>Mulberry32.expected());
+  testCommonRandom(() => Mulberry32(), ()=>Mulberry32.expected());
 
   checkReferenceFiles(() => Mulberry32(1), 'a');
+  checkReferenceFiles(() => Mulberry32(0), 'b');
+  checkReferenceFiles(() => Mulberry32(777), 'c');
+  checkReferenceFiles(() => Mulberry32(1081037251), 'd');
+
+  // write_mulberry32("a", 1);
+  // write_mulberry32("b", 0);
+  // write_mulberry32("c", 777);
+  // write_mulberry32("d", 1081037251u);
+
+  test("values for JS generator", () {
+
+    final exp = [ 1118692146,
+      3456687457,
+      2323025554,
+      2964572940,
+      4890715,
+      3511320825,
+      48751514,
+      452846334,
+      1703291702,
+      2881671998 ];
+
+    final random = Mulberry32(99);
+    expect(List.generate(10, (_)=>random.nextRaw32()), exp);
+
+  });
+
+
+  //print(List.generate(3, (_)=>random.nextRaw32()));
+
+  // 1118692146
+  // 3456687457
+  // 2323025554
 
   //Mulberry32(1).nextRaw32();
 
